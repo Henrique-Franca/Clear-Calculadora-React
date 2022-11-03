1a87d0747cef4bfc5cb9f148ba6f2ef4a18797da
Henrique França <henriqsf@gmail.com>
2022-11-03 12:23:03 -0300

    Componente Button 02 ...

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,21 @@ import "./Button.css";
 
 export default props=>{
 
+    let classes = 'button';
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+
     return (
     
-    <button className="button">{props.label}</button>
-    
+        <button 
+            onClick={e => props.click && props.click(e.target.innerHTML)}
+            className={classes}>
+
+            {props.label}
+
+        </button>
+        
     )
 
 }
